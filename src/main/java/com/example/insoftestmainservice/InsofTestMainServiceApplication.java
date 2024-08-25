968e7153cdbab6f50ca68e32e44a435dbe19f8b9
Khusrav837@gmail.com <Khusrav_2011@mail.ru>
2024-08-25 18:22:51 +0500

    first commit

---
+++ src/main/java/com/example/insoftestmainservice/InsofTestMainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.insoftestmainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsofTestMainServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsofTestMainServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/insoftestmainservice/InsofTestMainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insoftestmainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsofTestMainServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
